7d12a349a38d54b53900e6c874f320a36bc37b95
klee045 <leekaien98@gmail.com>
2025-06-21 22:14:26 +0800

    feat: implement checking of token expiry and refreshing if needed

---
+++ middleware/token.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import { getAzureAccessToken } from "../controllers/auth.controller";
+
+/**
+ * Middleware to check expiry of access token
+ * @param req ExpressJS Request object
+ * @param res ExpressJS Response object
+ * @param next ExpressJS NextFunction object
+ * @returns
+ */
+export const checkAzureAccessToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("@@@@@@@@@@@@@@@ checking");
+    const currentTime = Date.now();
+    const expiresAt = parseInt(
+      process.env.AZURE_ACCESS_TOKEN_EXPIRES_AT || "0",
+      10
+    );
+
+    // get new token if current is expired
+    if (currentTime >= expiresAt || !process.env.AZURE_ACCESS_TOKEN) {
+      console.log("Token expired or doesn't exist. Refreshing...");
+      const tokenResponse = await getAzureAccessToken();
+
+      // update env variables storing access token and expiry
+      if (tokenResponse) {
+        process.env.AZURE_ACCESS_TOKEN = tokenResponse.accessToken;
+        process.env.AZURE_ACCESS_TOKEN_EXPIRES_AT =
+          tokenResponse.expiresAt.toString();
+      } else {
+        res.status(500).json({ message: "Error obtaining new access token." });
+        return;
+      }
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error obtaining new access token", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
