0c24a3b899a0849ea813db55507d0de8e364df57
klee045 <leekaien98@gmail.com>
2025-06-21 11:50:16 +0800

    feat: implement retrieval of access token from Microsoft on app start

---
+++ models/auth.model.ts
@@ -0,0 +1,6 @@
+export interface AzureOAuthResponse {
+  token_type: string;
+  expires_in: string;
+  ext_expires_in: string;
+  access_token: string;
+}
--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import { app } from "./app";
+import { getAzureAccessToken } from "./auth/auth";
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
+  getAzureAccessToken(); // initialize Azure Access Token for Microsoft Graph API usage on app start
+
   console.log(`Example app listening on port ${port}`);
 });
