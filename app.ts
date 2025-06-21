aa0e6a46d2f613b54a72a101efd159584df3edfc
klee045 <leekaien98@gmail.com>
2025-06-21 15:41:50 +0800

    init ms graph client instead of axios due to issues with encoding url params

--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import { checkAzureAccessToken } from "./middleware/token";
+import { Client } from "@microsoft/microsoft-graph-client";
 
 export const app = express();
+export const msGraphClient = Client.init({
+  authProvider: (done) => {
+    done(null, process.env.AZURE_ACCESS_TOKEN || "");
+  },
+});
 
 app.use(checkAzureAccessToken);
 
