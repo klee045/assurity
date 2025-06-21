9161f2cacbeb76ad2aa42a42059eec81161c4e7a
klee045 <leekaien98@gmail.com>
2025-06-21 16:14:49 +0800

    init mongodb boilerplate connection

--- server.ts
+++ server.ts
@@ -1,10 +1,20 @@
+import mongoose from "mongoose";
 import { app } from "./app";
 import { getAzureAccessToken } from "./controllers/auth";
+import { mongoDb } from "./db/mongo";
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
   getAzureAccessToken(); // initialize Azure Access Token for Microsoft Graph API usage on app start
+
+  mongoDb
+    .then((db: mongoose.Mongoose) => {
+      console.log(`MongoDB connected. Version: ${db.version}`);
+    })
+    .catch((err: any) => {
+      console.log("Error connecting to MongoDB:", err);
+    });
 
   console.log(`Example app listening on port ${port}`);
 });
